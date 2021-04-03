33c633a24ddce0a81757ac79fbe634b94e6f0879
DavidVegaC <david.vega.vc@gmail.com>
2021-04-03 15:50:20 -0500

    add config gateway-services

--- shopping-service/src/main/java/academy/digitallab/store/shopping/model/Customer.java
+++ shopping-service/src/main/java/academy/digitallab/store/shopping/model/Customer.java
@@ -1,9 +1,11 @@
 package academy.digitallab.store.shopping.model;
 
+import lombok.Builder;
 import lombok.Data;
 
 
 @Data
+@Builder
 public class Customer {
     private Long id;
     private String numberID;
